8e82d3c73fe7b1441c8f0d5a75ed29b5574c46d9
ItsRishky <143787045+ItsRishky@users.noreply.github.com>
2025-08-07 22:36:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ io.on("connection", function(socket) {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
